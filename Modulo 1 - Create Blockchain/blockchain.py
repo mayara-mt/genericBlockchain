cb5c309c2a3aeefefea82a0c676651927921f557
Mayara Madeira Trevisol <mayara.trevisol@udesc.br>
2022-04-26 08:36:27 -0300

    Add funcionalidade que verifica se a blockchain é valida

--- Modulo 1 - Create Blockchain/blockchain.py
+++ Modulo 1 - Create Blockchain/blockchain.py
@@ -100,4 +100,16 @@ def get_chain():
     return jsonify(response), 200
 
 
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    
+    if is_valid:
+        response = {'message': 'Blockchain is valid!'}
+    else:
+        response = {'message': 'Blockchain is NOT valid!'}
+    
+    return jsonify(response), 200
+
+
 app.run(host='0.0.0.0', port=5000)
